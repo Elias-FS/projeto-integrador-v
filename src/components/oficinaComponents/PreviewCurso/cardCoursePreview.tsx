f6fb9329c8380977527c027346e2bf323daef64f
Emílio Biasi <ejbiasi@hotmail.com>
2023-10-30 10:37:44 -0300

    ajustando mudança de estado do CardPreview

--- src/components/oficinaComponents/PreviewCurso/cardCoursePreview.tsx
+++ src/components/oficinaComponents/PreviewCurso/cardCoursePreview.tsx
@@ -22,15 +22,23 @@ const CardCoursePreview: React.FC<CardUserProps> = ({
     <Card className="h-full w-full overflow-hidden mx-15 shadow-2xl border-gray-400">
       <CardHeader className="p-0">
         <img
-          src={src !== null ? src : "https://igp.rs.gov.br/themes/modelo-noticias/images/outros/GD_imgSemImagem.png"}
+          src={
+            src !== null
+              ? src
+              : "https://igp.rs.gov.br/themes/modelo-noticias/images/outros/GD_imgSemImagem.png"
+          }
           alt="Imagem do usuário"
           className="aspect-video object-cover"
         />
       </CardHeader>
       <CardContent className="h-full w-full border-gray-100">
-        <CardTitle className="mx-2 pt-4 font-bold">{titulo}</CardTitle>
+        <CardTitle className="mx-2 pt-4 font-bold">
+          {titulo !== "" ? titulo : "Título"}
+        </CardTitle>
         <p className="mt-2 mx-2 text-zinc-600 font-semibold text-xl">
-          {descricao}
+          {descricao !== ""
+            ? descricao
+            : "Descrição. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in lectus volutpat, laoreet neque eget, convallis tortor."}
         </p>
       </CardContent>
     </Card>
