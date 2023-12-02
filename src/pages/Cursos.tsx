743a29b77b5e0de3fe16a34aa522f4b6b426ff31
kevinsilvadev <kivim2018@gmail.com>
2023-12-02 01:07:20 -0300

    Consumindo cursos aprovados

--- src/pages/Cursos.tsx
+++ src/pages/Cursos.tsx
@@ -1,50 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Sidebar } from "@/components/ui/Sidebar";
 import { Navbar } from "@/components/ui/Navbar";
 import CardCourse from "@/components/ui/Cards/cardCourse";
+import CursoService from "@/services/curso.service";
+import { Curso } from "@/models/curso";
 
 const Cursos: React.FC = () => {
-  const cursos = [
-    {
-      id: "031cccd8-0a96-44e6-b8ee-d5247de06767",
-      titulo: "Curso de Python",
-      descricao: "Descrição Python",
-      data_nascimento: "11/11/2023",
-      academia: "Academia A",
-      capa: "https://www.interviewbit.com/blog/wp-content/uploads/2023/05/Artboard-1-copy-2.jpg",
-      listaDeSlides: [
-        { slideType: "em branco" },
-        { slideType: "em branco" },
-        { slideType: "em branco" },
-      ],
-    },
-    {
-      id: "031cccd8-0a96-44e6-b8ee-d5247de06768",
-      titulo: "Curso de C++",
-      descricao: "Descrição C++",
-      data_nascimento: "11/11/2023",
-      academia: "Academia B",
-      capa: "https://www.interviewbit.com/blog/wp-content/uploads/2023/05/Artboard-1-copy-2.jpg",
-      listaDeSlides: [
-        { slideType: "verdadeiro ou falso" },
-        { slideType: "verdadeiro ou falso" },
-        { slideType: "verdadeiro ou falso" },
-      ],
-    },
-    {
-      id: "031cccd8-0a96-44e6-b8ee-d5247de06769",
-      titulo: "Curso de TypeScript",
-      descricao: "Descrição TypeScript",
-      data_nascimento: "11/11/2023",
-      academia: "Academia C",
-      capa: "https://www.interviewbit.com/blog/wp-content/uploads/2023/05/Artboard-1-copy-2.jpg",
-      listaDeSlides: [
-        { slideType: "somente midia" },
-        { slideType: "somente midia" },
-        { slideType: "somente midia" },
-      ],
-    }
-  ];
+
+  const [ cursos , setCursos ] = useState<Curso[]>([]);
+  const cursosPendentes:Array<Curso> = []
+
+  useEffect(() => {
+
+    CursoService.listarCursos().then(
+      (response) => {
+          response.data.forEach(element => {
+            if(element.validado != false) {
+              cursosPendentes.push(element)
+            }
+          });
+          setCursos(cursosPendentes);
+      },
+      (error) => {
+        const _content =
+          (error.response && error.response.data) 
+          error.message 
+          error.toString();
+      }
+    );
+  }, []);
+
+  
+  // const cursos = [
+  //   {
+  //     id: "031cccd8-0a96-44e6-b8ee-d5247de06767",
+  //     titulo: "Curso de Python",
+  //     descricao: "Descrição Python",
+  //     data_nascimento: "11/11/2023",
+  //     academia: "Academia A",
+  //     capa: "https://www.interviewbit.com/blog/wp-content/uploads/2023/05/Artboard-1-copy-2.jpg",
+  //     listaDeSlides: [
+  //       { slideType: "em branco" },
+  //       { slideType: "em branco" },
+  //       { slideType: "em branco" },
+  //     ],
+  //   },
+  //   {
+  //     id: "031cccd8-0a96-44e6-b8ee-d5247de06768",
+  //     titulo: "Curso de C++",
+  //     descricao: "Descrição C++",
+  //     data_nascimento: "11/11/2023",
+  //     academia: "Academia B",
+  //     capa: "https://www.interviewbit.com/blog/wp-content/uploads/2023/05/Artboard-1-copy-2.jpg",
+  //     listaDeSlides: [
+  //       { slideType: "verdadeiro ou falso" },
+  //       { slideType: "verdadeiro ou falso" },
+  //       { slideType: "verdadeiro ou falso" },
+  //     ],
+  //   },
+  //   {
+  //     id: "031cccd8-0a96-44e6-b8ee-d5247de06769",
+  //     titulo: "Curso de TypeScript",
+  //     descricao: "Descrição TypeScript",
+  //     data_nascimento: "11/11/2023",
+  //     academia: "Academia C",
+  //     capa: "https://www.interviewbit.com/blog/wp-content/uploads/2023/05/Artboard-1-copy-2.jpg",
+  //     listaDeSlides: [
+  //       { slideType: "somente midia" },
+  //       { slideType: "somente midia" },
+  //       { slideType: "somente midia" },
+  //     ],
+  //   }
+  // ];
   
   console.log(localStorage.getItem('token'))
   return (
